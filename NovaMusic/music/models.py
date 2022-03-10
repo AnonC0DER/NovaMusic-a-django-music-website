c0b5d13a601714d6e27c64fd43d4b487f3203724
Hesam N <AnonCODER@tutanota.com>
2022-03-11 00:41:04 +0330

    Start the project & music app created
    - Start the project
    - Music model created

---
+++ NovaMusic/music/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+# from artist.models import Artist
+# from genre.models import Genre
+# from album.models import Album
+
+class Music(models.Model):
+    '''Music model'''
+    class Meta:
+        verbose_name = 'Music'
+        verbose_name_plural = 'Music'
+
+    title = models.CharField(max_length=220)
+    thumbnail = models.ImageField(upload_to='SongsThumbnails/', default='SongsThumbnails/default.jpg')
+    # artists = models.ManyToManyField(Artist)
+    # album = models.ForeignKey(Album, on_delete=models.CASCADE, null=True, blank=True)
+    # genre = models.ManyToManyField(Genre)
+    song = models.FileField(upload_to='Songs/', null=True, blank=True)
+    song_url = models.CharField(max_length=350, null=True, blank=True)
+    lyrics = models.TextField(null=True, blank=True)
+    slug = models.SlugField(null=True, blank=True)
+    published = models.BooleanField(default=False)
+    single_track = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
