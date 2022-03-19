5394f46897f37dfc6f2b848608670a4334c58397
Hesam N <AnonCODER@tutanota.com>
2022-03-19 20:18:10 +0330

    MusicComment created
    - MusicComment inherit from main Comment model
    - A few tests added to test MusicComment model
    - set_song_duration signal added to set song duration

--- NovaMusic/music/admin.py
+++ NovaMusic/music/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from music.models import Music
+from music.models import Music, MusicComment
 
 class MusicAdmin(admin.ModelAdmin):
     '''Music admin page'''
@@ -8,4 +8,13 @@ class MusicAdmin(admin.ModelAdmin):
     list_filter = ['published']
     readonly_fields = ['get_album_name', 'id']
 
+
+class MusicCommentAdmin(admin.ModelAdmin):
+    '''Music comment admin page'''
+    list_display = ['comment_title', 'active', 'owner', 'music']
+    search_fields = ['owner__email', 'owner__name', 'music__title']
+    list_filter = ['active']
+
+
 admin.site.register(Music, MusicAdmin)
+admin.site.register(MusicComment, MusicCommentAdmin)
--- NovaMusic/music/tests/test_MusicComment_model.py
+++ NovaMusic/music/tests/test_MusicComment_model.py
@@ -1,59 +1,59 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from album.models import Album, AlbumComment
+from music.models import Music, MusicComment
 
 User = get_user_model()
 
-class TestAlbumCommentModel(TestCase):
-    '''Tests to test AlbumComment model'''
+class TestMusicCommentModel(TestCase):
+    '''Tests to test MusicComment model'''
     def create_comment(self):
         '''Create and return comment object'''
-        return AlbumComment.objects.create(
+        return MusicComment.objects.create(
             owner=self.user_obj,
-            album=self.album_obj,
+            music=self.music_obj,
             body='Test comment',
         )
 
     def setUp(self):
         self.user_obj = User.objects.create(email='test@gmail.com', password='testpass123')
         self.user_obj_2 = User.objects.create(email='test2@gmail.com', password='testpass123')
-        self.album_obj = Album.objects.create(title='Test album obj')
+        self.music_obj = Music.objects.create(title='Test music obj')
 
-    def test_comment_on_album_obj(self):
-        '''Test comment on a album object'''
+    def test_comment_on_music_obj(self):
+        '''Test comment on a music object'''
         comment_obj = self.create_comment()
-        filter_comment_obj = AlbumComment.objects.filter(active=True)
+        filter_comment_obj = MusicComment.objects.filter(active=True)
 
         self.assertFalse(comment_obj.active)
         self.assertIsNone(comment_obj.reply)
         self.assertIsNotNone(comment_obj.body)
         self.assertIsNotNone(comment_obj.created)
         self.assertEqual(comment_obj.owner, User.objects.get(email='test@gmail.com'))
-        self.assertIn(comment_obj.album, Album.objects.all())
+        self.assertIn(comment_obj.music, Music.objects.all())
         self.assertEqual(filter_comment_obj.count(), 0)
 
     def test_reply_comment(self):
         '''Test reply on a comment'''
         comment_obj = self.create_comment()
-        reply_obj = AlbumComment.objects.create(
+        reply_obj = MusicComment.objects.create(
             owner=self.user_obj_2,
-            album=self.album_obj,
+            music=self.music_obj,
             body='Test reply',
             reply=comment_obj
         )
 
         self.assertEqual(comment_obj.replies.all().count(), 1)
-        self.assertEqual(self.album_obj.albumcomment_set.all().count(), 2)
-        self.assertIn(reply_obj, self.album_obj.albumcomment_set.all())
+        self.assertEqual(self.music_obj.musiccomment_set.all().count(), 2)
+        self.assertIn(reply_obj, self.music_obj.musiccomment_set.all())
         self.assertEqual(reply_obj.owner, self.user_obj_2)
         self.assertEqual(reply_obj.body, comment_obj.replies.first().body)
 
     def test_reply_on_invalid_comment(self):
         '''Test reply on an invalid comment'''
         with self.assertRaises(ValueError):
-            AlbumComment.objects.create(
+            MusicComment.objects.create(
                 owner=self.user_obj_2,
-                Album=self.album_obj,
+                music=self.music_obj,
                 body='Test reply',
                 reply=123
             )
